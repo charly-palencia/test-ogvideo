019d1e46c1820345b7988910c6e956c64d33e2af
Charly palencia <ingcharlypalencia@gmail.com>
2025-02-09 11:07:17 -0500

    redirect video

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          // Basic redirect
+          {
+            source: '/rfile',
+            destination: '/video2',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
