9086505169cbe5030dc21aa09645f53c83c1ca8d
Charly palencia <ingcharlypalencia@gmail.com>
2025-02-09 12:55:23 -0500

    Ttst

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      crossOrigin: 'anonymous',
+    crossOrigin: 'anonymous',
     async redirects() {
         return [
           // Basic redirect
--- src/app/wvideo/page.js
+++ src/app/wvideo/page.js
@@ -2,73 +2,49 @@ const Video = '/embed';
 const Thumbnail = '/video.png';
 const baseUrl = "https://test-ogvideo-sepia.vercel.app";
 
-// export async function generateMetadata({ params }, parent) {
-//     const title = "My video without extension";
+export async function generateMetadata({ params }, parent) {
+    const title = "My video without extension";
 
-//     const description = "video example";
+    const description = "video example";
 
-//     return {
-//         metadataBase: new URL(baseUrl),
-//         title,
-//         description,
-//         openGraph: {
-//             title,
-//             description,
-//             url: baseUrl,
-//             images: [
-//                 {
-//                     url: Thumbnail,
-//                     secureUrl: Thumbnail,
-//                     width: 1200,
-//                     height: 630,
-//                     alt: "Preview image for Dan Mugh's Blog",
-//                 },
-//             ],
-//             videos: [
-//                 {
-//                     url: `${baseUrl}${Video}`,
-//                     secureUrl: `${baseUrl}${Video}`,
-//                     width: 1280,
-//                     height: 720,
-//                     type: "text/html",
-//                 },
-//             ],
-//             twitter: {
-//                 card: "player",
-//                 title,
-//                 site: baseUrl,
-//                 images: [
-//                     {
-//                         url: `${baseUrl}${Thumbnail}`,
-//                         width: 1280,
-//                         height: 720,
-//                         alt: "Preview image for Dan Mugh's Blog",
-//                     },
-//                 ],
-//                 player: {
-//                     stream: `${baseUrl}${Video}`,
-//                     width: 1280,
-//                     height: 720
-//                 }
-//             },
-//             type: "video.other",
-//             siteName: "Dan Mugh's Blog",
-//         },
-//     };
-// }
+    return {
+        metadataBase: new URL(baseUrl),
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            url: baseUrl,
+            images: [
+                {
+                    url: Thumbnail,
+                    secureUrl: Thumbnail,
+                    width: 1200,
+                    height: 630,
+                    alt: "Preview image for Dan Mugh's Blog",
+                },
+            ],
+            videos: [
+                {
+                    url: `https://www.youtube.com/embed/dgimjn-HoEQ`,
+                    secureUrl: `https://www.youtube.com/embed/dgimjn-HoEQ`,
+                    width: 1280,
+                    height: 720,
+                    type: "text/html",
+                    alt: "Preview image for Dan Mugh's Blog",
+                },
+            ],
+            type: "video.other",
+            siteName: "Dan Mugh's Blog",
+        },
+    };
+}
 
 
 
 export default function VideoPage() {
   return (
         <html>
-            <head>
-                <meta name="twitter:card" content="player" />
-                <meta name="twitter:player" content={`${baseUrl}${Video}`} />
-                <meta name="twitter:player:stream" content={`${baseUrl}${Video}`} />
-                <meta name="twitter:player:width" content="1280" />
-                <meta name="twitter:player:height" content="720" />
-            </head>
             <body>
                 <div className="h-screen bg-black flex justify-center items-center">
                     without extension Video preview
